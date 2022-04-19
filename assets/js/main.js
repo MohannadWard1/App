3bb5826b49f391230dee22668820e2adff00df61
Mohannad Ward <Mohannadward246@gmail.com>
2022-04-19 06:39:08 +0200

    Features section built

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,31 @@ close.addEventListener("click", function () {
   mobileLinks.classList.remove("open");
 });
 mobLis.forEach((li) => {
-  li.addEventListener("click", function () {
+  li.addEventListener("click", function (e) {
+    mobLis.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
     mobileLinks.classList.remove("open");
   });
 });
 free.addEventListener("click", function () {
   mobileLinks.classList.remove("open");
+});
+
+let btn = document.querySelector(".top");
+
+window.onscroll = function () {
+  if (window.scrollY >= 500) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
+  }
+};
+
+btn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
